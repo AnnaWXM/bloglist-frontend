bef5edbaea12c5fe772e04544307690b4ea4162c
AnnaWXM <xiaomininfi@gmail.com>
2024-04-21 15:48:03 +0300

    5.14

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders content when details are true', () => {
@@ -25,4 +26,24 @@ test('does not render content when details are false', () => {
 
   const element = screen.queryByText('Component testing is done with react-testing-library')
   expect(element).toBeNull()
+})
+
+test('shows URL and number of likes when details are shown', async() => {
+  const blog = {
+    title: 'Test Blog',
+    author: 'Test Author',
+    url: 'https://example.com',
+    likes: 10,
+    details: false
+  }
+
+  const mockHandler = vi.fn()
+
+  render(<Blog blog={blog} />)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('Show Details')
+  await userEvent.click(button)
+
+  expect(mockHandler.mock.calls).toHaveLength(1)
 })
